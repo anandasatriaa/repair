027f41bce62d462c026a11b1cc5919924f75fd95
anandasatriaa <anandasatriaf6@gmail.com>
2025-07-25 14:38:04 +0700

    fix: logo di halaman admin

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -53,8 +53,8 @@
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
-                <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                <a href="#" class="d-flex justify-content-center mx-auto mb-4">
+                    <img src="{{ asset('assets/img/logo.png') }}" width="150" alt="">
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
